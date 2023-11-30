ec815da89e10dd28795cc42b632b801699241fe3
김나현 <66942657+nahyun0912@users.noreply.github.com>
2023-11-30 19:07:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		long x = sc.nextLong();
+		long y = sc.nextLong();
+		long z = sc.nextLong();
+		System.out.println(x+y+z);
+	}
+
+}
+
