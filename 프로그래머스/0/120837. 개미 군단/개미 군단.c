d9ebeb45967bb650a24c2d6f089a90c808ab3b06
김나현 <66942657+nahyun0912@users.noreply.github.com>
2024-11-11 19:17:33 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int hp) {
+    int answer = 0;
+    answer = hp/5+(hp%5/3)+(hp%5%3);
+    
+    
+    return answer;
+}
